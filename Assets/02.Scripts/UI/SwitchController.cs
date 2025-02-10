7e8f7fe9bd8b7a76629f4c8d1a0df75d0e0655cc
Yoon Gun <dbsrjs0514@naver.com>
2025-02-10 11:17:38 +0900

    스위치 소리 구현

--- Assets/02.Scripts/UI/SwitchController.cs
+++ Assets/02.Scripts/UI/SwitchController.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(AudioSource))]
+
 public class SwitchController : MonoBehaviour
 {
     [SerializeField] private Image handleImage;
-
+    [SerializeField] private AudioClip clickSound;
+    
+    private Image _backgroundImage;
+    private AudioSource _audioSource;
     private RectTransform _handleRectTransform;
     private bool _isOn;
+
+    public delegate void OnSwitchChangedDelegate(bool isOn);
+    public OnSwitchChangedDelegate OnSwitchChanged;
+
+    private static readonly Color32 OnColor = new Color32(150, 150, 150, 255);
+    private static readonly Color32 OffColor = new Color32(90, 90, 90, 255);
     
     private void Awake()
     {
         _handleRectTransform = handleImage.GetComponent<RectTransform>();
+        _backgroundImage = GetComponent<Image>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
     {
-        SetOn(false);
+        _handleRectTransform.anchoredPosition = new Vector2(-15, 0);
+        _backgroundImage.color = OffColor;
+        _isOn = false;
     }
 
     private void SetOn(bool isOn)
@@ -27,12 +43,18 @@ public class SwitchController : MonoBehaviour
         if (isOn)
         {
             _handleRectTransform.DOAnchorPosX(15, 0.2f);
+            _backgroundImage.DOBlendableColor(OnColor, 0.2f);
         }
         else
         {
             _handleRectTransform.DOAnchorPosX(-15, 0.2f);
+            _backgroundImage.DOBlendableColor(OffColor, 0.2f);
         }
-
+        //효과음 재생
+        if(clickSound != null) _audioSource.PlayOneShot(clickSound);
+        
+        
+        OnSwitchChanged?.Invoke(isOn);
         _isOn = isOn;
     }
 
